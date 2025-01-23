52e1d55edbcdc9882973e36b6c44ff0cdfd2c396
Afnan A. <hafnan03@gmail.com>
2025-01-23 14:31:39 +0500

    totop cursor

--- app/components/Scrolltotop.tsx
+++ app/components/Scrolltotop.tsx
@@ -24,7 +24,7 @@ const Scrolltotop: React.FC = () => {
       {isVisible && (
         <button
           onClick={scrollToTop}
-          className="cursor-default fixed bottom-6 left-6 xxs:p-1 md:p-2 bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-gray-800 rounded hover:bg-gray-700 hover:text-gray-100 dark:hover:bg-gray-400 dark:hover:text-gray-900 transition-colors duration-200"
+          className="fixed bottom-6 left-6 xxs:p-1 md:p-2 bg-gray-800 dark:bg-gray-200 text-gray-200 dark:text-gray-800 rounded hover:bg-gray-700 hover:text-gray-100 dark:hover:bg-gray-400 dark:hover:text-gray-900 transition-colors duration-200"
           aria-label="Scroll to Top"
         >
           <MdArrowUpward />
