dfbf58da5b1ee0bb6a895b4c6af63fe9d4cf2d04
Afnan A. <hafnan03@gmail.com>
2025-02-06 22:26:15 +0500

    .

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -88,7 +88,6 @@ export default function Navbar() {
           rel="noopener noreferrer"
           className="relative px-3 xxs:py-1.5 md:py-1 xxs:text-xs md:text-sm font-bold bg-white text-gray-900 rounded-sm overflow-hidden group inline-flex items-center justify-center"
         >
-          {/* <button className="relative px-3 xxs:py-1.5 md:py-1 xxs:text-xs md:text-sm font-bold bg-white text-gray-900 rounded-sm overflow-hidden group"> */}
           <span className="relative z-10">
             <span className="xxs:hidden md:inline">
               el
@@ -102,7 +101,6 @@ export default function Navbar() {
           <span className="absolute inset-0 bg-gradient-to-r from-red-500 via-blue-500 to-red-500 opacity-0 group-hover:opacity-10 transition-opacity duration-300"></span>
           <span className="absolute -top-1 left-1/2 w-6 h-6 bg-red-500 rounded-full opacity-75 animate-wave"></span>
           <span className="absolute -bottom-1 right-1/2 w-6 h-6 bg-blue-500 rounded-full opacity-75 animate-wave"></span>
-          {/* </button> */}
         </a>
       </div>
       {/* Right Section (Contribute & Theme Toggle) */}
