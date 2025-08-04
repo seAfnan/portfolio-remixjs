44df72cc0bea2c89340b6e5a4db523d70f9ed7a6
Afnan A. <hafnan03@gmail.com>
2025-08-04 14:09:44 +0500

    comments error

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -109,15 +109,12 @@ export default function Navbar() {
           rel="noopener noreferrer"
           className="relative px-3 xxs:py-1.5 md:py-1 xxs:text-xs md:text-sm font-bold bg-white text-gray-900 rounded-sm overflow-hidden group inline-flex items-center justify-center gap-1"
         >
-          {/* Robot Icon */}
           <RiRobot2Fill className="text-red-400 text-base md:text-lg transition-transform duration-300 group-hover:scale-110 group-hover:-translate-y-0.5" />
 
-          {/* Text */}
           <span className="relative z-10" style={{marginTop: "2px"}}>
             <span className="xxs:hidden md:inline">Botshot</span>
           </span>
 
-          {/* Soft background hover effect */}
           <span className="absolute inset-0 bg-gradient-to-r from-red-500 via-blue-500 to-red-500 opacity-0 group-hover:opacity-10 transition-opacity duration-300"></span>
         </a>*/}
       </div>
