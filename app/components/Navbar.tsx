9e70f97993eadfa9066f0774fa662973497837fe
Afnan A. <hafnan03@gmail.com>
2025-09-24 10:15:02 +0500

    .

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { FaHandshake } from "react-icons/fa";
 import { Link, useLocation } from "@remix-run/react";
 import { IoIosGitMerge } from "react-icons/io";
 import { GrProjects } from "react-icons/gr";
-import { RiRobot2Fill } from "react-icons/ri"; // Add this import at the top
+import { LuBookDashed } from "react-icons/lu"; // Add this import at the top
 
 export default function Navbar() {
   const { theme, toggleTheme } = useTheme();
@@ -109,7 +109,7 @@ export default function Navbar() {
           rel="noopener noreferrer"
           className="relative px-3 xxs:py-1.5 md:py-1 xxs:text-xs md:text-sm font-bold bg-white text-gray-900 rounded-sm overflow-hidden group inline-flex items-center justify-center gap-1"
         >
-          <RiRobot2Fill className="text-red-400 text-base md:text-lg transition-transform duration-300 group-hover:scale-110 group-hover:-translate-y-0.5" />
+          <LuBookDashed className="text-red-400 text-base md:text-lg transition-transform duration-300 group-hover:scale-110 group-hover:-translate-y-0.5" />
 
           <span className="relative z-10" style={{marginTop: "2px"}}>
             <span className="xxs:hidden md:inline">LifeDiary</span>
